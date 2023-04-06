ca282afb3b54a17b3373a1223c006f27d91e4c84
Hugo.M <h.maestracci83@gmail.com>
2023-04-06 13:31:19 +0200

    quick update

--- Assets/StarterAssets/InputSystem/StarterAssetsInputs.cs
+++ Assets/StarterAssets/InputSystem/StarterAssetsInputs.cs
@@ -1,4 +1,6 @@
+using System;
 using UnityEngine;
+using UnityEngine.AI;
 #if ENABLE_INPUT_SYSTEM
 using UnityEngine.InputSystem;
 #endif
@@ -15,16 +17,24 @@ namespace StarterAssets
 
 		[Header("Movement Settings")]
 		public bool analogMovement;
-
+		private Camera _mainCamera;
+		private NavMeshAgent _navMeshAgent;
+		[Range(1f, 50f)]
+		[SerializeField] private float _rayMaxDistance = 20f;
+		[SerializeField] LayerMask _groundLayer;
+		
+		public NavMeshAgent NavMeshAgent => _navMeshAgent;
+		
 		[Header("Mouse Cursor Settings")]
 		public bool cursorLocked = true;
 		public bool cursorInputForLook = true;
 
-#if ENABLE_INPUT_SYSTEM
-		public void OnMove(InputValue value)
+		private void Start()
 		{
-			MoveInput(value.Get<Vector2>());
+			_mainCamera = Camera.main;    
+			_navMeshAgent = GetComponent<NavMeshAgent>();
 		}
+#if ENABLE_INPUT_SYSTEM
 
 		public void OnLook(InputValue value)
 		{
@@ -45,20 +55,35 @@ namespace StarterAssets
 		}
 #endif
 
+		
+		private void Update()
+		{
+			if (!Input.GetMouseButtonDown(0))
+				return;
+        
+			Ray cameraRay = _mainCamera.ScreenPointToRay(Input.mousePosition);
 
+			if (Physics.Raycast(cameraRay, out var hitInfo, _rayMaxDistance, _groundLayer.value))
+			{
+				var newMoveDirection = hitInfo.point;
+				move = newMoveDirection;
+				_navMeshAgent.SetDestination(newMoveDirection);
+			}
+		}
+		
 		public void MoveInput(Vector2 newMoveDirection)
 		{
-			move = newMoveDirection;
+			//move = newMoveDirection;
 		} 
 
 		public void LookInput(Vector2 newLookDirection)
 		{
-			look = newLookDirection;
+			//look = newLookDirection;
 		}
 
 		public void JumpInput(bool newJumpState)
 		{
-			jump = newJumpState;
+			//jump = newJumpState;
 		}
 
 		public void SprintInput(bool newSprintState)
--- Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
+++ Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
@@ -133,6 +133,8 @@ namespace StarterAssets
             {
                 _mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
             }
+            
+            _input.NavMeshAgent.speed = MoveSpeed;
         }
 
         private void Start()
@@ -264,15 +266,15 @@ namespace StarterAssets
                     RotationSmoothTime);
 
                 // rotate to face input direction relative to camera position
-                transform.rotation = Quaternion.Euler(0.0f, rotation, 0.0f);
+                //transform.rotation = Quaternion.Euler(0.0f, rotation, 0.0f);
             }
 
 
             Vector3 targetDirection = Quaternion.Euler(0.0f, _targetRotation, 0.0f) * Vector3.forward;
 
             // move the player
-            _controller.Move(targetDirection.normalized * (_speed * Time.deltaTime) +
-                             new Vector3(0.0f, _verticalVelocity, 0.0f) * Time.deltaTime);
+            //_controller.Move(targetDirection.normalized * (_speed * Time.deltaTime) +
+                             //new Vector3(0.0f, _verticalVelocity, 0.0f) * Time.deltaTime);
 
             // update animator if using character
             if (_hasAnimator)

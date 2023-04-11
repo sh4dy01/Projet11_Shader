ed0971224011aab40cb509f546375acd1424bc4e
Lum0o <ProGpipart@gmail.com>
2023-04-11 12:33:19 +0200

    material Reflection

--- Assets/Scripts/WeatherManager.cs
+++ Assets/Scripts/WeatherManager.cs
@@ -6,14 +6,57 @@ public class WeatherManager : MonoBehaviour
 {
     [SerializeField] private Material[] _materials;
     [SerializeField] private ParticleSystem _particleSystem;
-    void Start()
+    
+    [Header("Time values")]
+    [SerializeField] private float _minRainDuration;
+    [SerializeField] private float _maxRainDuration;
+    [SerializeField] private float _minRainInterval;
+    [SerializeField] private float _maxRainInterval;
+    
+    [SerializeField] private Light _directionalLight;
+    [SerializeField] private float _secondsPerDay = 60.0F;
+    [SerializeField] private float _timer = 0.0F;
+
+    private float _timeBeforeRain;
+    private bool _isRaining;
+    private float _rainDuration;
+
+    private void Awake()
     {
-        
+        _directionalLight.useColorTemperature = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        _timer += Time.deltaTime;
+
+        float lightAngle = _timer / _secondsPerDay * 360.0F;
+        _directionalLight.transform.rotation = Quaternion.Euler(lightAngle, 0.0F, 0.0F);
+        _directionalLight.intensity = Mathf.Clamp(Mathf.Sin(lightAngle * Mathf.Deg2Rad) * 10.0F, 0.0F, 1.0F);
+        _directionalLight.colorTemperature = Mathf.Sin(lightAngle * Mathf.Deg2Rad) * (6800 - 2400) + 2400;
+        Rain();
+    }
+
+    private void Rain()
+    {
+        if (_isRaining)
+        {
+            if(_rainDuration <= 0)
+            {
+                _particleSystem.Stop();
+                _timeBeforeRain = Random.Range(_minRainInterval,_maxRainInterval);
+            }
+            _rainDuration -= Time.deltaTime;
+        }
+        else
+        {
+            if(_timeBeforeRain <= 0)
+            {
+                _particleSystem.Play();
+                _rainDuration = Random.Range(_minRainDuration,_maxRainDuration);
+                foreach (var material in _materials) material.SetFloat("Smoothness", 5f);
+            }
+            _timeBeforeRain -= Time.deltaTime;
+        }
     }
 }

7c269bbb3f6923162b9881767dfd4806d40f91e0
Hugo.M <h.maestracci83@gmail.com>
2023-04-13 20:56:36 +0200

    remove debug log

--- Assets/Scripts/PlayerDamageZone.cs
+++ Assets/Scripts/PlayerDamageZone.cs
@@ -4,9 +4,6 @@ public class PlayerDamageZone : DamageZone
 {
     protected override void OnTriggerStay(Collider other)
     {
-        Debug.Log(_canDamage);
-        Debug.Log(other.gameObject.name);
-        
         if (!_canDamage || !other.gameObject.CompareTag("Enemy")) return;
         
         Debug.Log("Damaging enemy");
